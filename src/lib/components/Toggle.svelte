5801966120cd8a7b25a21e2f7811a43dafe2e22b
haelp <haelpf@gmail.com>
2024-10-29 18:47:54 -0400

    why tf am i getting an error for email verification? i don't understand

--- src/lib/components/Toggle.svelte
+++ src/lib/components/Toggle.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
   export let checked = false;
+	export let color = "bg-orange-400";
 </script>
 
 <label class="flex cursor-pointer items-center">
   <div class="relative h-6 w-10 rounded-full bg-slate-600">
     <input type="checkbox" bind:checked class="peer hidden" />
     <div
-      class="absolute left-[2px] top-1/2 h-5 w-5 -translate-y-1/2 rounded-full bg-orange-400 transition-all peer-checked:left-[18px]"
+      class="absolute left-[2px] top-1/2 h-5 w-5 -translate-y-1/2 rounded-full {color} transition-all peer-checked:left-[18px]"
     ></div>
   </div>
 </label>
--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -2,3 +2,4 @@ export { default as Collapsible } from "./Collapsible.svelte";
 export { default as BubbleButton } from "./BubbleButton.svelte";
 export { default as Toggle } from "./Toggle.svelte";
 export { default as Skeleton } from "./Skeleton.svelte";
+export { default as Footer } from "./Footer.svelte";
