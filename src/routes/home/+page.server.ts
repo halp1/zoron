4dd56fa01c53e94e4df93fea40c408968fe4c9d3
halp <haelpf@gmail.com>
2024-11-05 16:19:06 -0500

    one other fix

--- src/routes/home/+page.server.ts
+++ src/routes/home/+page.server.ts
@@ -8,7 +8,10 @@ export const load: PageServerLoad = async ({ locals: { auth }, request: { url } 
   const page = uri.searchParams.get("page");
   if (page !== "home") {
     if (session?.user?.settings?.home?.default) {
-      return redirect(302, `/home/${session.user.settings.home.default}`);
+      return redirect(
+        302,
+        `/home/${session.user.settings.home.default === "home" ? "?page=home" : session.user.settings.home.default}`
+      );
     }
   }
   return {};
