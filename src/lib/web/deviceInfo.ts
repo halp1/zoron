3f2281cd63c06f9086f167bc4e18b894c3400c8e
halp <haelpf@gmail.com>
2024-11-09 21:39:34 -0500

    get bruhed

--- src/lib/web/deviceInfo.ts
+++ src/lib/web/deviceInfo.ts
@@ -68,9 +68,9 @@ export const getDeviceInfo = async (): Promise<Device> => {
 };
 
 export const isIOS = () =>
-  (typeof navigator !== "undefined" &&
-    ["iPad Simulator", "iPhone Simulator", "iPod Simulator", "iPad", "iPhone", "iPod"].includes(
-      navigator.platform
-    )) ||
-  // iPad on iOS 13 detection
-  (navigator.userAgent.includes("Mac") && "ontouchend" in document);
+  typeof navigator !== "undefined" &&
+  (["iPad Simulator", "iPhone Simulator", "iPod Simulator", "iPad", "iPhone", "iPod"].includes(
+    navigator.platform
+  ) ||
+    // iPad on iOS 13 detection
+    (navigator.userAgent.includes("Mac") && "ontouchend" in document));
