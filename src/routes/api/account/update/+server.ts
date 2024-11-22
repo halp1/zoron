f8e780cdcc4d86d2dea323be04304b0e5421dffc
halp <haelpf@gmail.com>
2024-11-21 22:12:46 -0500

    beta, bugfixes?, security measures

--- src/routes/api/account/update/+server.ts
+++ src/routes/api/account/update/+server.ts
@@ -1,8 +1,11 @@
 import { aspen } from "$lib/aspen";
 import { adapter } from "$lib/auth";
+import { CONSTANTS } from "$lib/constants";
+import { html, text, validEmail } from "$lib/email";
 import { api } from "$lib/server";
 import type { Session } from "$lib/types";
 
+import { MAILGUN_KEY } from "$env/static/private";
 import _ from "lodash";
 
 import { defaultSettings } from "../settings/defaults";
@@ -28,16 +31,43 @@ export const POST: RequestHandler = async ({ locals: { auth }, request }) => {
     const account = await aspen.authenticate(data.username, data.password);
     const email = await aspen.email(account.cookie);
     if (email !== session?.user?.email) {
-      return api.error(
-        `This is not your account (email mismatch: ${email} vs ${session?.user?.email})`,
-        401
-      );
+      const msg = `This is not your account. This incedent has been reported`;
+      const to = email,
+        from = "system@mail.haelp.dev";
+      const form = new FormData();
+      form.append("from", `${CONSTANTS.name} system <${from}>`);
+      form.append("to", to);
+      form.append("subject", `Sign in to ${CONSTANTS.name} (https://${CONSTANTS.url})`);
+      if (validEmail(to)) {
+        form.append(
+          "text",
+          `${session?.user?.email} just tried to create a ${CONSTANTS.name} account using your MyFollet Aspen credentials. Foward this email to "26stu282@lexingtonma.org" for more information. It is likely that this person knows your login information, which means it needs to be changed.`
+        );
+      } else {
+        throw new Error("An invalid email was found on the account.");
+      }
+
+      const res = await fetch(`https://api.mailgun.net/v3/mail.haelp.dev/messages`, {
+        method: "POST",
+        headers: {
+          Authorization: `Basic ${btoa(`api:${MAILGUN_KEY}`)}`
+        },
+        body: form
+      });
+
+      if (!res.ok) throw new Error("Mailgun error: " + (await res.text()));
+
+      throw new Error(msg);
     }
 
     const encrypted = aspen.encrypt(email, data.username, data.password);
 
     const activity = await aspen.activity(account.cookie);
-    const ids = activity.merged.map((item) => btoa(item.id + item.sscid)).slice(1);
+    const ids = activity.merged
+      .map((item) =>
+        btoa(("id" in item ? item.id : item.oid) + ("sscid" in item ? item.sscid : item.date))
+      )
+      .slice(1);
 
     await adapter.updateUser!({
       id: session.user.id!,
