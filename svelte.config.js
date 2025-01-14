4fb87f9e5dd4ef154ccc8c0745327564cab0747b
haelp <haelpf@gmail.com>
2025-01-14 14:32:48 -0500

    save

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
-import adapter from "@sveltejs/adapter-node";
+// import adapter from "@sveltejs/adapter-node";
+import adapter from "@sveltejs/adapter-vercel";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -6,7 +7,7 @@ const config = {
   preprocess: vitePreprocess({ script: true }),
 
   kit: {
-    adapter: adapter({precompress: true, }),
+    adapter: adapter({ precompress: true }),
     csrf: { checkOrigin: false }
   }
 };
