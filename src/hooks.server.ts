835420a37dc60304b837762571b4bdc82e4b8778
haelp <haelpf@gmail.com>
2025-03-29 13:29:29 -0400

    starting jobs

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,8 +2,14 @@ import { handle as authHandle } from "$lib/auth";
 
 import type { Handle } from "@sveltejs/kit";
 
+import { jobs } from "./jobs";
+
 export const handle: Handle = async (params) => {
   if (params.event.url.pathname === "/api/admin/impersonate")
     return await params.resolve(params.event);
   return await authHandle(params);
 };
+
+if ("jobs" in globalThis) ((globalThis as any).jobs as typeof jobs).stop();
+
+jobs.init();
---
+++ src/jobs/index.ts
@@ -0,0 +1,22 @@
+import type { Job } from "./base";
+import { Notifier } from "./notifier";
+
+export namespace jobs {
+  export const jobs: InstanceType<typeof Job>[] = [];
+  export const init = () => {
+    jobs.splice(0, jobs.length, new Notifier());
+
+    for (const job of jobs) {
+      job.start();
+    }
+  };
+
+  export const stop = () => {
+    for (const job of jobs) {
+      job.stop();
+    }
+    jobs.splice(0, jobs.length);
+  };
+}
+
+(globalThis as any).jobs = jobs;
