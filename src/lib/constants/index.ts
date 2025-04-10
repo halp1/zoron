9ed41092f8b824a006c9dec7edc28e34ecb25a39
halp <haelpf@gmail.com>
2025-04-10 16:14:31 -0400

    fix a small build issue

--- src/lib/constants/index.ts
+++ src/lib/constants/index.ts
@@ -1,8 +1,5 @@
-import { execSync } from "node:child_process";
-
 export namespace CONSTANTS {
   export const name = "Zoron";
   export const url = "zoron.app";
   export const passkeyExpiration = 1000 * 60 * 5;
-	export const commit = execSync('git rev-parse --short HEAD').toString().trim();
 }
