627c061badef5c498e985ea5915f57c7cafe1050
halp <haelpf@gmail.com>
2025-05-27 21:30:35 -0400

    oops forgot to commit the logs

--- src/routes/account/password/+page.svelte
+++ src/routes/account/password/+page.svelte
@@ -28,7 +28,7 @@
     const { dismiss } = toast.loading("Verifying credentials...");
     submitting = true;
 
-    const res = await requests.post<any>("/api/account/password", {
+    const res = await requests.post<any>("/api/acco unt/password", {
       password
     });
     dismiss();
---
+++ src/routes/api/admin/logs/+server.ts
@@ -0,0 +1,39 @@
+import { zoron } from "$lib";
+import { api } from "$lib/server";
+
+import fsSync from "node:fs";
+import fs from "node:fs/promises";
+import path from "node:path";
+
+import type { RequestHandler } from "./$types";
+import { logger } from "$lib/logs";
+
+export const GET: RequestHandler = async ({ locals: { auth }, url }) => {
+  const session = await auth();
+
+  if (session?.user?.role !== "admin") throw api.error("Forbidden", 403);
+
+  const chunk = url.searchParams.get("start");
+	const size= url.searchParams.get("size");
+	if (chunk && isNaN(parseInt(chunk))) {
+		throw api.error("Invalid start parameter", 400);
+	}
+	if (size && isNaN(parseInt(size))) {
+		throw api.error("Invalid size parameter", 400);
+	}
+
+  if (!fsSync.existsSync(logger.LOG_PATH)) {
+    await fs.mkdir(path.dirname(logger.LOG_PATH), { recursive: true });
+    await fs.writeFile(logger.LOG_PATH, "");
+  }
+
+  const res = await logger.reversePaginateFile(
+    logger.LOG_PATH,
+    chunk ? parseInt(chunk) : undefined,
+    size ? parseInt(size) : undefined
+  );
+
+  return api.json({
+    logs: res
+  });
+};
