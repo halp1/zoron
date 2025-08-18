b9f9c7e35dd62b9b4e6afea1c6488b6922d5437e
halp <haelpf@gmail.com>
2025-08-18 18:39:59 -0400

    Fix some build issues

--- pro/scripts/generateKeys.ts
+++
@@ -1,7 +0,0 @@
-import webpush from "web-push";
-
-// Generate VAPID keys
-const vapidKeys = webpush.generateVAPIDKeys();
-
-console.log("Public Key:", vapidKeys.publicKey);
-console.log("Private Key:", vapidKeys.privateKey);
