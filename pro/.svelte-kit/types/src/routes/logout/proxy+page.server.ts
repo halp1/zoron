43f4eb4dbd547421fa0ecd58e96ec719ebc79036
halp <haelpf@gmail.com>
2025-08-18 11:03:48 -0400

    Remove .svelte-kit

--- pro/.svelte-kit/types/src/routes/logout/proxy+page.server.ts
+++
@@ -1,9 +0,0 @@
-// @ts-nocheck
-import { redirect } from "@sveltejs/kit";
-
-import type { PageServerLoad } from "./$types";
-
-export const load = async ({ locals: { auth } }: Parameters<PageServerLoad>[0]) => {
-  if (!(await auth())?.user?.email) return redirect(302, "/login");
-  return {};
-};
