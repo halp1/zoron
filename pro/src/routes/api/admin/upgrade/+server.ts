ad5025a90408bb8282e098acfbc23d07c6cd3a48
halp <haelpf@gmail.com>
2025-08-24 19:06:43 -0400

    server upgrade fix

--- pro/src/routes/api/admin/upgrade/+server.ts
+++ pro/src/routes/api/admin/upgrade/+server.ts
@@ -1,4 +1,5 @@
 import { api } from "@zoron/common/server";
+
 import { execSync, spawn } from "child_process";
 import path from "path";
 import process from "process";
@@ -9,7 +10,13 @@ export const POST = async ({ locals: { auth } }) => {
   const session = await auth();
   if (session?.user?.role !== "admin") throw api.error("Unauthorized", 403);
 
-  const p = spawn(path.join(process.cwd(), "prod.sh"), {
+  const p = spawn(path.resolve(process.cwd(), "../pro", "prod.sh"), {
+    detached: true,
+    stdio: "ignore"
+  });
+  p.unref();
+
+  const p2 = spawn(path.resolve(process.cwd(), "../main", "prod.sh"), {
     detached: true,
     stdio: "ignore"
   });
