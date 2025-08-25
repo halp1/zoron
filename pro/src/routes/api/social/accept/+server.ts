d2a7cd9b35c6097fd8bec1695ba467d98d51d967
halp <haelpf@gmail.com>
2025-08-25 09:41:02 -0400

    fix pro routes missing

--- pro/src/routes/api/social/accept/+server.ts
+++ pro/src/routes/api/social/accept/+server.ts
@@ -2,8 +2,9 @@ import { adapter } from "@zoron/common/auth";
 import { api } from "@zoron/common/server";
 import type { Relationship } from "@zoron/common/types";
 
-import type { RequestHandler } from "./$types";
 import type { User } from "@auth/sveltekit";
+
+import type { RequestHandler } from "./$types";
 
 export const POST: RequestHandler = async ({ locals: { auth }, request }) => {
   const session = await auth();
@@ -14,7 +15,7 @@ export const POST: RequestHandler = async ({ locals: { auth }, request }) => {
 
   if (!body.id) throw api.error("Missing id", 400);
 
-  const user = await adapter.getUser!(body.id);
+  const user: (User & { id: string }) | null = await adapter.getUser!(body.id);
 
   if (!user) throw api.error("User not found", 404);
 
@@ -25,7 +26,9 @@ export const POST: RequestHandler = async ({ locals: { auth }, request }) => {
   if (!existing || existing.type !== "request-outgoing")
     throw api.error("No request to approve", 404);
 
-  const self = (await adapter.getUser!(session.user.id))!;
+  const self: User & { id: string } = (await adapter.getUser!(
+    session.user.id
+  ))!;
 
   await adapter.updateUser!({
     id: user.id,
@@ -34,7 +37,7 @@ export const POST: RequestHandler = async ({ locals: { auth }, request }) => {
         []),
       { user: session!.user!.id, type: "friend", since: new Date() }
     ]
-  });
+  } satisfies User as any);
 
   await adapter.updateUser!({
     id: self.id,
@@ -42,7 +45,7 @@ export const POST: RequestHandler = async ({ locals: { auth }, request }) => {
       ...(self.relationships?.filter((rel) => rel.user !== user.id) ?? []),
       { user: user.id, type: "friend", since: new Date() }
     ]
-  });
+  } satisfies User as any);
 
   return api.json({
     message: "Friend request accepted",
