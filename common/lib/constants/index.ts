f25d45b65d057ceb422cd9b2a13763a81b35c82b
halp <haelpf@gmail.com>
2025-08-26 09:39:47 -0400

    Fix full schedule on a small mobile device

--- common/lib/constants/index.ts
+++ common/lib/constants/index.ts
@@ -3,4 +3,6 @@ export namespace CONSTANTS {
   export const url = "zoron.app";
   export const passkeyExpiration = 1000 * 60 * 5;
 	export const proAccountAge = 15;
+
+	export const scheduleSensitivity = 50;
 }
