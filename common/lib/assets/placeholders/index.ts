d07d0c709090430dd06b52210bcea73fa64c871a
halp <haelpf@gmail.com>
2025-09-06 15:09:37 -0400

    small change

--- common/lib/assets/placeholders/index.ts
+++ common/lib/assets/placeholders/index.ts
@@ -7,11 +7,14 @@ import img5 from "./5.png";
 export const placeholderImages = [img1, img2, img3, img4, img5];
 
 let lastPlaceholderIndex = 0;
-export const randomPlaceholderImage = (): string => {
-  const randomIndex = Math.floor(Math.random() * placeholderImages.length);
-  if (randomIndex === lastPlaceholderIndex) {
-    return randomPlaceholderImage();
-  }
-  lastPlaceholderIndex = randomIndex;
-  return placeholderImages[randomIndex];
+export const randomPlaceholderImage = (date: Date): string => {
+  const dayOfYear = Math.floor(
+    (date.getTime() - new Date(date.getFullYear(), 0, 0).getTime()) /
+      1000 /
+      60 /
+      60 /
+      24
+  );
+  const index = dayOfYear % placeholderImages.length;
+  return placeholderImages[index];
 };
